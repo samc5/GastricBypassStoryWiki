d3be665c0c8b616c174912291d5ed9a17ccc26f2
SNirloy <yolrinidas@gmail.com>
2022-11-12 23:13:06 -0500

    Saving progress for now. Creation of new stories available

--- app/__init__.py
+++ app/__init__.py
@@ -16,14 +16,18 @@ app.secret_key = os.urandom(32)
 #SQLite set up
 tbl_hndl.seed()
 
+
 @app.route("/", methods = ["POST", "GET"])
-def home_page():
+def entrance():
     #print(request.form)
+    print(session)
     notice = ""
     if ("login" in request.form):
         #print ("Logging in " + request.form["username"])
         if (tbl_hndl.checkLogin( request.form["username"],  request.form["password"])):
-            notice = "Logged In"
+            session["username"] = request.form["username"]
+            session["password"] = request.form["password"]
+            username = session["username"]
         else: 
             notice = "Wrong Username or Password"
 
@@ -32,12 +36,32 @@ def home_page():
             notice = "Username Taken"
         else:
             notice = "User Registered. Please Log In."
+    if (len(session) > 0):
+        return redirect("/homepage")
     return render_template("login_and_register.html", message = notice)
+
+@app.route("/homepage", methods=["POST", "GET"])
+def home_page():
+	print(session)
+	matrix = tbl_hndl.list_of_pages()
+	print (matrix)
+	table_form = matrix
+
+	return render_template("landing.html", table = table_form)
+
+@app.route("/make_story", methods=["POST", "GET"])
+def make_story():
+	title = request.form["title"]
+	text = request.form["opening"]
+	if (len(title) > 0 and len(text) > 0):
+		tbl_hndl.start_story(title, text, session["username"])
+	return redirect("/homepage")
 
 @app.route("/logout")
 def logout():
   session.pop("username")
   session.pop("password")
+  username = ""
   print(session)
   return redirect("/")
 
